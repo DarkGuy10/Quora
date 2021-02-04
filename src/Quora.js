259a7aa5a19c05ae479968a515c27572df4b74ef
DarkGuy10 <HarperBurn@protonmail.ch>
2021-02-04 11:08:06 +0530

    minor fix

--- src/Quora.js
+++ src/Quora.js
@@ -11,22 +11,21 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname + 'icons/logo.ico'
   });
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://quora.com')  
   mainWindow.maximize()
+  //mainWindow.setMenu(null)
   mainWindow.webContents.on('new-window', (event, url) => {
     event.preventDefault()
     mainWindow.loadURL(url)
   })
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
-app.on('ready', async () => {
+app.on('ready', async () => { // Loading extensions
   // await session.defaultSession.loadExtension(path.join(__dirname, 'DarkTheme'))
   //await session.defaultSession.loadExtension(path.join(__dirname, 'DarkTheme'))
 })
@@ -47,6 +46,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
