a79acfc315f896035814b350db554b3473b62642
DallasOnBackup <78465594+DallasOnBackup@users.noreply.github.com>
2021-02-04 00:13:56 -0600

    possible bug fix
    should fix css not applying

--- src/DarkTheme/dark.js
+++ src/DarkTheme/dark.js
@@ -1,6 +1,9 @@
 // grab original css
 var originCSS = document.body.style;
 
+// initial check
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) document.body.style.src = "DarkTheme.css";
+
 // Even listener for changes in system preference
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', detect => {
     if (window.matchMedia('prefers-color-scheme: dark').matches) {
