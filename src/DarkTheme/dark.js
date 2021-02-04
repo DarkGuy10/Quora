56e09d330dacd5bacbc627e9f2460c84b51f7025
DallasOnBackup <78465594+DallasOnBackup@users.noreply.github.com>
2021-02-04 00:21:01 -0600

    BUG FIX
    *SIGH*

--- src/DarkTheme/dark.js
+++ src/DarkTheme/dark.js
@@ -2,13 +2,13 @@
 var originCSS = document.body.style;
 
 // initial check
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) document.body.style.src = "DarkTheme.css";
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) document.body.style.src = "/DarkTheme.css";
 
 // Even listener for changes in system preference
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', detect => {
     if (window.matchMedia('prefers-color-scheme: dark').matches) {
         document.body.style = originCSS;
      } else {
-        document.body.style.src = "DarkTheme.css";
+        document.body.style.src = "/DarkTheme.css";
 }
 });
