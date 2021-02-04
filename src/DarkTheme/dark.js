6ed527c024d4f89ed8f03233f513b15ecaaa8c3c
DallasOnBackup <78465594+DallasOnBackup@users.noreply.github.com>
2021-02-04 00:25:47 -0600

    file resize

--- src/DarkTheme/dark.js
+++ src/DarkTheme/dark.js
@@ -1,9 +1,6 @@
 // grab original css
 var originCSS = document.body.style;
 
-// initial check
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) document.body.style.src = "/DarkTheme.css";
-
 // Even listener for changes in system preference
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', detect => {
     if (window.matchMedia('prefers-color-scheme: dark').matches) {
@@ -12,3 +9,6 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', det
         document.body.style.src = "/DarkTheme.css";
 }
 });
+
+// initial check
+detect();
