6e58a41fcb9c95ef789882e4951342350d051929
DallasOnBackup <78465594+DallasOnBackup@users.noreply.github.com>
2021-02-12 05:24:58 -0600

    fixed crap

--- src/adBlocker.js
+++ src/adBlocker.js
@@ -2,7 +2,7 @@ function adCheck() {
 var ads = document.getElementsByClassName("q-click-wrapper qu-display--inline-block qu-tapHighlight--white qu-cursor--pointer qu-hover--textDecoration--underline";
 var adCount = ads.length;
 for (i = adCount; i != 0; i--) {
-	ads[i].innerHTML = null;
+	ads[i].parentElement.innerHTML = null;
 } }
 var refreshID = setInterval(adCheck, 1000);
 window.onbeforeunload = function() {
